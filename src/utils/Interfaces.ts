88fcf722913bb7acaa27fa0139493ecf9fdabcf9
unknown <marcospla413@gmail.com>
2024-05-12 10:43:04 -0300

    add(-): create interface

---
+++ src/utils/Interfaces.ts
@@ -0,0 +1,11 @@
+export interface Character {
+    name: string;
+    eye_color: string;
+    gender: string;
+    birthday_year?: number;
+    hair_color?: string;
+    height?: number;
+    skin_color?: string;
+    mass?: number;
+  }
+  
