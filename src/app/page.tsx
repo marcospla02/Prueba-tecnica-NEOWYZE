4f4cb13a79ae3adb572802cc32a437fbd9eec418
unknown <marcospla413@gmail.com>
2024-05-12 15:13:17 -0300

    Refactor(-): Clear comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-import Link from "next/link";
+import LandingPage from '@/assets/landing-page.png';
 import LayoutMain from "@/components/layout";
-import LandingPage from '@/assets/landing-page.png'
 
 const Home = () => {
   return (
@@ -25,18 +24,6 @@ const Home = () => {
               ¡Espero que te guste el proyecto tanto como a
               mí y que estés tan emocionado como yo por esta oportunidad! 
             </p>  
-            {/* <div className="flex justify-center md:justify-start">
-              <div className="m-4">
-                <Link href="/peliculas">
-                  <a className="text-[colors.buttonTextDecoration] hover:underline">Películas</a>
-                </Link>
-              </div>
-              <div className="m-4">
-                <Link href="/personajes">
-                  <a className="text-[colors.buttonTextDecoration] hover:underline">Personajes</a>
-                </Link>
-              </div>
-            </div> */}
           </div>
           <div className="hidden md:block">
             <img src={LandingPage.src} alt="Tu imagen" className="mx-auto rounded-lg shadow-lg" />
