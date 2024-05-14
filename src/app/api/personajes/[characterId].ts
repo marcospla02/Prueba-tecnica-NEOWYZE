73cb850859101020fc3fb94323120b723cc33dd8
unknown <marcospla413@gmail.com>
2024-05-14 12:25:37 -0300

    Add(-): Create route character Id

---
+++ src/app/api/personajes/[characterId].ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export async function GET(id: string) {
+  try {
+    const url = `https://swapi.dev/api/people/${id}`;
+
+    const infoUrl = await axios(url);
+
+    return infoUrl.data;
+  } catch (error: any) {
+    console.log({ Message: error.message });
+  }
+}
