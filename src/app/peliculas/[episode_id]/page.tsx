f7122e336dcc7b9ff1d74e6ed97fc7e9ae48a615
unknown <marcospla413@gmail.com>
2024-05-14 15:44:05 -0300

    Fix(-): Back navigation

--- src/app/peliculas/[episode_id]/page.tsx
+++ src/app/peliculas/[episode_id]/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { GET } from "@/app/api/peliculas/[filmId]";
+import BackButton from "@/components/BackButton/BackButton";
 import FilmDetailCard from "@/components/FilmCard/FilmDetailCard";
 import { Films } from "@/utils/Interfaces";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const MoveisDetail = ({ params }: { params: { episode_id: string } }) => {
   const [films, setFilms] = useState<Films>();
+  const router = useRouter();
 
   useEffect(() => {
     const fetchFilms = async () => {
@@ -17,7 +20,13 @@ const MoveisDetail = ({ params }: { params: { episode_id: string } }) => {
     fetchFilms();
   }, []);
 
-  return <FilmDetailCard film={films} />;
+  return (
+    <>
+      <BackButton handleGoBack={() => router.back()} />
+
+      <FilmDetailCard film={films} />
+    </>
+  );
 };
 
 export default MoveisDetail;
--- src/app/personajes/[character_id]/page.tsx
+++ src/app/personajes/[character_id]/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { GET } from "@/app/api/personajes/[characterId]";
+import BackButton from "@/components/BackButton/BackButton";
 import CharacterDetailCard from "@/components/CharacterCard/CharacterDetailCard";
 import { Character } from "@/utils/Interfaces";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const ChracterDetail = ({ params }: { params: { character_id: string } }) => {
   const [character, setCharacter] = useState<Character>();
+  const router = useRouter();
 
   useEffect(() => {
     const fetchCharacter = async () => {
@@ -18,7 +21,12 @@ const ChracterDetail = ({ params }: { params: { character_id: string } }) => {
     fetchCharacter();
   }, []);
 
-  return <CharacterDetailCard character={character} />;
+  return (
+    <>
+      <BackButton handleGoBack={() => router.back()} />
+      <CharacterDetailCard character={character} />
+    </>
+  );
 };
 
 export default ChracterDetail;
