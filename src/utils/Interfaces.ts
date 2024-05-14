da554cdefc8b75c6f8d0f80b1d6fae6961256b18
unknown <marcospla413@gmail.com>
2024-05-14 12:39:49 -0300

    Fix(-): Fixed attributes

--- src/utils/Interfaces.ts
+++ src/utils/Interfaces.ts
@@ -1,11 +1,22 @@
 export interface Character {
-    name: string;
-    eye_color: string;
-    gender: string;
-    birthday_year?: number;
-    hair_color?: string;
-    height?: number;
-    skin_color?: string;
-    mass?: number;
-  }
-  
+  name: string;
+  eye_color: string;
+  gender: string;
+  birthday_year?: number;
+  hair_color?: string;
+  height?: number;
+  skin_color?: string;
+  mass?: number;
+  img: string;
+  id: string;
+  url?: string;
+}
+
+export interface Films {
+  title: string;
+  episode_id: number;
+  director: string;
+  characters: [];
+  url:string;
+  img: string;
+}
